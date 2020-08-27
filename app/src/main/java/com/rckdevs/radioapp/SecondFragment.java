a2d243f94be0e0e0923a928132175019165f8ca4
rckdevs <rckdevs@gmail.com>
2020-08-26 23:05:11 -0500

    Primera version del Radio App

--- app/src/main/java/com/rckdevs/radioapp/SecondFragment.java
+++ app/src/main/java/com/rckdevs/radioapp/SecondFragment.java
@@ -1,9 +1,18 @@
 package com.rckdevs.radioapp;
 
+import android.annotation.SuppressLint;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,24 +20,152 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
 
+    private ImageView imagePlayPause;
+    private TextView texCurrentTime, textTotalDuration;
+    private SeekBar playerSeekBar;
+    private MediaPlayer mediaPlayer;
+    private Handler handler = new Handler();
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
+
+        imagePlayPause = view.findViewById(R.id.imagePlayPause);
+        texCurrentTime =  view.findViewById(R.id.textCurrentTime);
+        textTotalDuration =  view.findViewById(R.id.textTotalDuration);
+        playerSeekBar =  view.findViewById(R.id.playerSeekerBar);
+
+        playerSeekBar.setMax(100);
+
+        mediaPlayer = new MediaPlayer();
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                }
+                playerSeekBar.setProgress(0);
+                imagePlayPause.setImageResource(R.drawable.ic_play);
+                texCurrentTime.setText(R.string.zero);
+                textTotalDuration.setText(R.string.zero);
+                mediaPlayer.reset();
+                //prepareMediaPlayer("");
+
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+
+        imagePlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()){
+                    handler.removeCallbacks(updater);
+                    mediaPlayer.pause();
+                    imagePlayPause.setImageResource(R.drawable.ic_play);
+                } else {
+                    mediaPlayer.start();
+                    imagePlayPause.setImageResource(R.drawable.ic_pause);
+                    updateSeekBar();
+                }
+
+            }
+        });
+
+        prepareMediaPlayer("http://infinityandroid.com/music/good_times.mp3");
+
+        playerSeekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SeekBar seekBar = (SeekBar) view;
+                int playPosition = (mediaPlayer.getDuration() / 100) * seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                texCurrentTime.setText(milliSecondsToTimer(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+                playerSeekBar.setSecondaryProgress(i);
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                playerSeekBar.setProgress(0);
+                imagePlayPause.setImageResource(R.drawable.ic_play);
+                texCurrentTime.setText(R.string.zero);
+                textTotalDuration.setText(R.string.zero);
+                mediaPlayer.reset();
+                prepareMediaPlayer("");
+            }
+        });
+
+    }
+
+    private void prepareMediaPlayer(String URL){
+        try{
+            mediaPlayer.setDataSource(URL); // URL of Music File
+            mediaPlayer.prepare();
+            textTotalDuration.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+        } catch (Exception ex){
+            //Toast.makeText(this, ex.getMessage().toString(), Toast.LENGTH_SHORT).show(); //LINEA CON ERROR
+        }
+    }
+
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            updateSeekBar();
+            long currentDuration = mediaPlayer.getCurrentPosition();
+            texCurrentTime.setText(milliSecondsToTimer(currentDuration));
+        }
+    };
+
+    private void updateSeekBar(){
+        if(mediaPlayer.isPlaying()){
+            playerSeekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
+            handler.postDelayed(updater, 1000);
+        }
+    }
+
+    private String milliSecondsToTimer(long milliSeconds){
+        String timerString = "";
+        String secondsString;
+
+        int hours = (int) (milliSeconds / (1000 * 60 * 60));
+        int minutes = (int) (milliSeconds % (1000 * 60 * 60)) / (1000 * 60);
+        int seconds = (int) ((milliSeconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+
+        if (hours > 0){
+            timerString = hours + ":";
+        }
+
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = "" + seconds;
+        }
+
+        timerString = timerString + minutes + ":" + secondsString;
+
+        return timerString;
     }
 }
